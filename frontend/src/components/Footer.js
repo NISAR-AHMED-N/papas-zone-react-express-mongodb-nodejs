53d1340d346ba0681dfc4840d405120196add07c
Nisar Ahmed N <nisarsyed372@gmail.com>
2025-06-16 13:41:51 -0700

    first commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return  <footer className="py-1 bg-dark">
+                <p className="text-center text-white mt-1 ">
+                    &copy; 2025 PAPA's ZONE All rights reserved
+                </p>
+            </footer>
+}
